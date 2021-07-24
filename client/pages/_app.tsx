64c45f214c66fe2c3073243be8cc35cdd9fefd69
flolu <loflude@gmail.com>
2021-07-24 07:27:02 +0200

    setup prettier for client

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from "next/app";
+import type {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+  return <Component {...pageProps} />
 }
-export default MyApp;
+export default MyApp
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <main>
       <h1>Hello world</h1>
     </main>
-  );
+  )
 }
